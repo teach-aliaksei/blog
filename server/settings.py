9bc345260137a0a07b06b5ab458e63846904f8b4
teach-aliaksei <ximik_animeshnik@mail.ru>
2022-08-21 19:25:05 +0300

    commit

--- server/settings.py
+++ server/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-9wmi43c)21o&p6j^-e9yd*81+gr216ydbm%^ngi+i^ae8v)@s*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
